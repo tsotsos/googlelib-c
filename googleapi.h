720081e02b86ffd755177daa44bf8e48343dbc99
Georgios Tsotsos <tsotsos@gmail.com>
2015-10-11 22:32:09 +0300

    MERGE API with GOOGLE header
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- googleapi.h
+++ googleapi.h
@@ -31,76 +31,8 @@
 #ifndef GOOGLEAPI_H_
 #define GOOGLEAPI_H_
 #include "google.h"
-/**
- * @brief Holding json info
- * This struct is holding name/value type of Json
- * response from google.
- * 
- * @param name: Name of response
- * @param value: Value of response
- *
- */
-typedef struct {
-  char * name;
-  char * value;
-  size_t length;
-} Json;
 
-/**
- * @brief Reads a file
- * @param filename:  The filename
- *
- * Reads a file to buffer *char
- **/
-char * ReadFile(char * filename);
-
-/**
- * @brief Parses a Json style string
- * @param json:  Json style string
- * @param value:  The wanted value
- *
- * Finds a value based on its name in a Json string
- **/
-char* parseJson(char *json, char *value);
-
- /**
- * @brief Find a value of config
- * @param string:  Config string
- * @param value:  The wanted value
- *
- * Finds a value based on its name in a Config string
- **/
-char * getValue(char * string, char * value);
-
-/**
- * @brief Get the settings from config file
- * @param filename:  Config filename
- *
- * Stores the settings from config file to a config
- * struct.
- **/
-config getSettings(char *filename);
-
-/**
- * @brief Saves settings to file
- * @param filename:  Config filename
- * @param name: Name of value to add o replace
- * @param value:  The value
- *
- * Saves/replace the wanted value by its name (preferable to config file)
- **/
-void setSetting(char * filename, char *name , char *value );
-
-/**
- * @brief Handles response from google api.
- * @param response:  response string
- *
- * Stores the google response if it is type of "application/json"
- * to an 'array' of structs (Json).
- * TODO: implement HTTP response error handling in case the response is
- * not "application/json".
- **/
-Json * parseResponse(char * response);
+char * GoogleDrive (char * token);
 #endif // GOOGLEAPI_H_
 
 
